418c0a0a4bac8aa972e33deb0d98700692b66f40
opencourse2020 <akaaboune@gmail.com>
2025-07-30 11:28:10 +0100

    add landing page

--- bizanalytic/profiles/models.py
+++ bizanalytic/profiles/models.py
@@ -63,9 +63,7 @@ class User(GuardianUserMixin, AbstractUser):
 
     @property
     def is_Member(self):
-        return hasattr(self, "Member")
-
-
+        return hasattr(self, "member")
 
     class Meta(AbstractUser.Meta):
         permissions = (

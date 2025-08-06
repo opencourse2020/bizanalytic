b4454ffb9963bb76f63b88e0d815852d6397efb4
opencourse2020 <akaaboune@gmail.com>
2025-08-06 22:42:26 +0100

    Add Stripe Payment

--- bizanalytic/logiflex/views.py
+++ bizanalytic/logiflex/views.py
@@ -40,7 +40,7 @@ class IndexView(TemplateView):
 
 
 class SampleAdvancedReportView(TemplateView):
-    template_name = "logiflex/report.html"
+    template_name = "logiflex/samplereport.html"
 
 
 class NewsletterCreateView(UserPassesTestMixin, CreateView):

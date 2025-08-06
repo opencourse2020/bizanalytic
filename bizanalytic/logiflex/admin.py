19cac84f042b76b788e650e166922313d8535be1
opencourse2020 <akaaboune@gmail.com>
2025-08-06 20:05:51 +0100

    Add Stripe Payment

--- bizanalytic/logiflex/admin.py
+++ bizanalytic/logiflex/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+model_objects = (models.Blog_logiflex,
+                 models.NewsLetter_logiflex,
+                 models.NewsLetter_logiflex_subscription,
+                 models.LogiFlexClient,
+                 models.LogiflexReport,
+                 models.PricingPlan,
+                 models.ServicePayment,
+                 models.RequestedCall
+)
+
+for m in model_objects:
+    admin.site.register(m)

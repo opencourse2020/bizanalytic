d55e41f4165db12a69d37de21978c21041835dce
opencourse2020 <akaaboune@gmail.com>
2025-08-12 21:34:07 +0100

    Add Stripe Payment

--- bizanalytic/static/vendor/transport/assets/js/load.js
+++ bizanalytic/static/vendor/transport/assets/js/load.js
@@ -159,9 +159,9 @@ $("#generate_full_rp").click(function (){
                             var message = result.submessage;
                             $("#loadingstate").hide();
                             if (message) {
-                                $("#report-message").html('<div class="alert alert-success d-flex align-items-center" role="alert"><svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg><div>' +
-                                    message + '</div>');
-                                $("#report-message").show();
+                                // $("#report-message").html('<div class="alert alert-success d-flex align-items-center" role="alert"><svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg><div>' +
+                                //     message + '</div>');
+                                // $("#report-message").show();
                                 var f = $("#toast_report");
                                 var a = new bootstrap.Toast(f);
                                 $("#message_body").html(message);

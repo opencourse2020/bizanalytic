d24fc866564324a325edac9c6c7c316badb0c9f4
opencourse2020 <akaaboune@gmail.com>
2025-08-12 20:46:38 +0100

    Add Stripe Payment

--- bizanalytic/static/vendor/transport/assets/js/load.js
+++ bizanalytic/static/vendor/transport/assets/js/load.js
@@ -160,7 +160,8 @@ $("#generate_full_rp").click(function (){
                             $("#loadingstate").hide();
                             if (message) {
                                 $("#report-message").html('<div class="alert alert-success d-flex align-items-center" role="alert"><svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg><div>' +
-                                    message + '</div>')
+                                    message + '</div>');
+                                $("#report-message").show();
                             }
                         }
                     }

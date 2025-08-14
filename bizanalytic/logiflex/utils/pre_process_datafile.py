77651dad3b079958dddfd6513a183a1e5e1a0da0
opencourse2020 <akaaboune@gmail.com>
2025-08-14 19:13:23 +0100

    create summary report

--- bizanalytic/logiflex/utils/pre_process_datafile.py
+++ bizanalytic/logiflex/utils/pre_process_datafile.py
@@ -710,6 +710,9 @@ def test_validator(routefile, report, routefilename):
     # print("Origine cities:", orig_cities.columns)
     normalizer = CityStateNormalizer(orig_cities, us_cities)
     clean_df, review_df = normalizer.normalize()
+    print("clean_df")
+    print(clean_df.head(5))
+    print(clean_df.info())
     data = data.drop(['OriginCity', 'DestinationCity'], axis=1)
     data = pd.concat([data, clean_df], axis=0, ignore_index=True)
 

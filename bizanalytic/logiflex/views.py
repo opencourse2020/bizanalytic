7f1734e62498b5f84de2be9d4c1062a8b808cebc
opencourse2020 <akaaboune@gmail.com>
2025-08-15 01:28:17 +0100

    create summary report

--- bizanalytic/logiflex/views.py
+++ bizanalytic/logiflex/views.py
@@ -85,6 +85,8 @@ class ReportView(TemplateView):
             df = clean_data(report)
             df = calculate_kpis(df)
             carrier_stats = prepare_carrier_stats(df).reset_index()
+            print("carrier stats")
+            print(carrier_stats.head(5))
             kwargs["carrierstats"] = carrier_stats
         return super(ReportView, self).get_context_data(**kwargs)
 

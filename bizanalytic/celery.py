7a2a8744d87094e131bade06ef3dbc2c9c0f1048
opencourse2020 <akaaboune@gmail.com>
2025-08-17 13:51:10 +0100

    create summary report

---
+++ bizanalytic/celery.py
@@ -0,0 +1,13 @@
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
+
+celery_app = Celery('bizanalytic')
+celery_app.config_from_object('django.conf:config.settings.production', namespace='CELERY')
+celery_app.autodiscover_tasks()
+
+
+@celery_app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')

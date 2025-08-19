e0b03c887aeb4e27bdb151faf56cde443f7ce504
opencourse2020 <akaaboune@gmail.com>
2025-08-19 22:04:12 +0100

    create summary report

--- bizanalytic/profiles/views.py
+++ bizanalytic/profiles/views.py
@@ -164,7 +164,7 @@ class ProfileView(LoginRequiredMixin, RedirectView):
 
 class DispatchLoginView(RedirectView):
     def get_redirect_url(self, *args, **kwargs):
-        if hasattr(self.request.user.is_staff):
+        if self.request.user.is_staff:
             return reverse_lazy("logiflex:admin:reports")
         elif hasattr(self.request.user, "admin"):
             return reverse_lazy("logiflex:dashboard")

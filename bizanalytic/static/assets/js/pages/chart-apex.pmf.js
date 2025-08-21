d1beb04a5f21a4e9db459b93392a53f6f4169d63
opencourse2020 <akaaboune@gmail.com>
2025-08-21 19:51:28 +0100

    create summary report

--- bizanalytic/static/assets/js/pages/chart-apex.pmf.js
+++ bizanalytic/static/assets/js/pages/chart-apex.pmf.js
@@ -7,7 +7,7 @@ Website: http://www.seantheme.com/hud/
 
 var handleRenderApexChart = function(full_df) {
 	df = new dfd.DataFrame(full_df);
-	let carrierdata = df[['AvgFreightCost', 'OnTimeRate']];
+	let carrierdata = df[['CarrierName', 'AvgFreightCost', 'OnTimeRate']];
 	console.log(carrierdata);
 	let numcarrieres = df['CarrierName'].count()
 	console.log(numcarrieres);
@@ -17,6 +17,15 @@ var handleRenderApexChart = function(full_df) {
 	// 	}
     // console.log(`Index: ${row._index}, Col1: ${row.col1}, Col2: ${row.col2}, Col3: ${row.col3}`);
   	console.log(carrierscatterdata);
+
+
+	const seriesdata =[];
+
+	for (let i = 0; i < df.shape[0]; i++) {
+		cdata = {name: df.iloc({rows: [i]})["CarrierName"].values[0], data: [[df.iloc({rows: [i]})["AvgFreightCost"].values[0], df.iloc({rows: [i]})["AvgFreightCost"].values[OnTimeRate]]]};
+		seriesdata.push({cdata})
+	}
+	var options = {series: seriesdata};
 
 	// Apex = {
 	// 	title: {
@@ -99,11 +108,7 @@ var handleRenderApexChart = function(full_df) {
 			zoom: { enabled: true, type: 'xy' }
 		},
 		// colors: [app.color.theme, app.color.warning, 'rgba('+ app.color.bodyColorRgb + ', .5)'],
-		series: [
-			{ name: 'SAMPLE A', data: [[16.4, 5.4],[21.7, 2],[25.4, 3],[19, 2],[10.9, 1],[13.6, 3.2],[10.9, 7.4],[10.9, 0],[10.9, 8.2],[16.4, 0],[16.4, 1.8],[13.6, 0.3],[13.6, 0],[29.9, 0],[27.1, 2.3],[16.4, 0],[13.6, 3.7],[10.9, 5.2],[16.4, 6.5],[10.9, 0],[24.5, 7.1],[10.9, 0],[8.1, 4.7],[19, 0],[21.7, 1.8],[27.1, 0],[24.5, 0],[27.1, 0],[29.9, 1.5],[27.1, 0.8],[22.1, 2]] }, 
-			{ name: 'SAMPLE B', data: [[36.4, 13.4],[1.7, 11],[5.4, 8],[9, 17],[1.9, 4],[3.6, 12.2],[1.9, 14.4],[1.9, 9],[1.9, 13.2],[1.4, 7],[6.4, 8.8],[3.6, 4.3],[1.6, 10],[9.9, 2],[7.1, 15],[1.4, 0],[3.6, 13.7],[1.9, 15.2],[6.4, 16.5],[0.9, 10],[4.5, 17.1],[10.9, 10],[0.1, 14.7],[9, 10],[12.7, 11.8],[2.1, 10],[2.5, 10],[27.1, 10],[2.9, 11.5],[7.1, 10.8],[2.1, 12]] }, 
-			{ name: 'SAMPLE C', data: [[21.7, 3],[23.6, 3.5],[24.6, 3],[29.9, 3],[21.7, 20],[23, 2],[10.9, 3],[28, 4],[27.1, 0.3],[16.4, 4],[13.6, 0],[19, 5],[22.4, 3],[24.5, 3],[32.6, 3],[27.1, 4],[29.6, 6],[31.6, 8],[21.6, 5],[20.9, 4],[22.4, 0],[32.6, 10.3],[29.7, 20.8],[24.5, 0.8],[21.4, 0],[21.7, 6.9],[28.6, 7.7],[15.4, 0],[18.1, 0],[33.4, 0],[16.4, 0]] }
-		],
+		series: seriesdata,
 		xaxis: {
 			tickAmount: 10,
 			labels: {
@@ -113,7 +118,7 @@ var handleRenderApexChart = function(full_df) {
 		yaxis: { tickAmount: 7 }
 	}
 	var apexScatterChart = new ApexCharts(
-		document.querySelector('#CarrierCostReliabilityBubbleChart'),
+		document.querySelector('#CarrierCostReliabilityChart'),
 		apexScatterChartOptions
 	);
 	apexScatterChart.render();

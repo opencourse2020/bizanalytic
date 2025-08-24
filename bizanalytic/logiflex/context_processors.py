1b94d4e28b0382e4c39d26880379e56895cb64f5
opencourse2020 <akaaboune@gmail.com>
2025-08-24 13:40:11 +0100

    create summary report

--- bizanalytic/logiflex/context_processors.py
+++ bizanalytic/logiflex/context_processors.py
@@ -1,8 +1,9 @@
 from .models import *
 
+
 def client_active(request):
     clientactive = False
-    if request.user.is_authenticated:
+    if request.user.is_authenticated and not request.user.is_staff:
         payment = ServicePayment.objects.filter(client__user=request.user).first()
         clientactive = payment.is_active
     return {'clientactive': clientactive}

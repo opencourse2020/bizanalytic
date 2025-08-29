33d6c35557689f41a5073fe616b99eaed57d0b06
opencourse2020 <akaaboune@gmail.com>
2025-08-29 13:11:49 +0100

    create summary report

--- bizanalytic/static/assets/js/pages/chart-apex.pmf.js
+++ bizanalytic/static/assets/js/pages/chart-apex.pmf.js
@@ -64,6 +64,7 @@ var handleRenderApexChart = function(full_df, costpermile, df_driver) {
 		});
 		}
 	}
+	console.log("driverspeedmpg", driverspeedmpg);
 	const drivermpgmedian = dfdriver["MedianMPG"].mean();
 	const driverontimemedian = dfdriver["OnTimeRate"].median()*100;
 	const drivermedianspeed = dfdriver["MedianSpeed"].mean();

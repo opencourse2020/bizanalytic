8f04ceaabf37487b0ba29d00c32a6f02ee6d9dab
opencourse2020 <akaaboune@gmail.com>
2026-04-21 01:50:52 +0100

    new operations research module

--- bizanalytic/static/assets/js/pages/sample_apex_charts.js
+++ bizanalytic/static/assets/js/pages/sample_apex_charts.js
@@ -9,56 +9,20 @@ var handleRenderApexChart = function(full_df, costpermile, df_driver, costmiledr
 	df = new dfd.DataFrame(full_df);
 	dfdriver = new dfd.DataFrame(df_driver);
 
-	let group_df = "";
-	let driver_df = "";
 	if (costpermile !== "0"){
 		dfcost = new dfd.DataFrame(costpermile);
 		dfcostdirver = new dfd.DataFrame(costmiledriver);
 		dfcost.print();
 		dfcost.sortValues("CostPerMile", { inplace: true })
-		group_df = dfcost.groupby(["CarrierName"]);
-		driver_df = dfcostdirver.groupby(["DriverName"]);
 	}
 
 
 	const seriesdata =[];
-    const costdata =[];
 	for (let i = 0; i < df.shape[0]; i++) {
 		seriesdata.push({name: df.iloc({rows: [i]})["CarrierName"].values[0], data: [[df.iloc({rows: [i]})["AvgFreightCost"].values[0], df.iloc({rows: [i]})["OnTimeRate"].values[0]]]});
-        if (costpermile !== "0") {
-			let a = group_df.getGroup([df.iloc({rows: [i]})["CarrierName"].values[0]]);
-			const median = d3.quantile(a["CostPerMile"].values, 0.5);
-			const q1 = d3.quantile(a["CostPerMile"].values, 0.25);
-			const min = d3.min(a["CostPerMile"].values);
-			const max = d3.max(a["CostPerMile"].values);
-			const q3 = d3.quantile(a["CostPerMile"].values, 0.75);
-			costdata.push({x: df.iloc({rows: [i]})["CarrierName"].values[0], y: [min, q1, median, q3, max]})
-		}
+
 	}
 
-	for (let i = 0; i < dfdriver.shape[0]; i++) {
-		driverseriesdata.push({
-			name: dfdriver.iloc({rows: [i]})["DriverName"].values[0],
-			data: [[dfdriver.iloc({rows: [i]})["MedianMPG"].values[0], dfdriver.iloc({rows: [i]})["OnTimeRate"].values[0]*100]]
-		});
-		if (costpermile !== "0") {
-			driverspeedmpg.push({
-			name: dfdriver.iloc({rows: [i]})["DriverName"].values[0],
-			data: [[dfdriver.iloc({rows: [i]})["MedianMPG"].values[0], dfdriver.iloc({rows: [i]})["MedianSpeed"].values[0]]]
-		});
-			driverontimempg.push({
-			name: dfdriver.iloc({rows: [i]})["DriverName"].values[0],
-			data: [[dfdriver.iloc({rows: [i]})["OnTimeRate"].values[0]*100, dfdriver.iloc({rows: [i]})["MedianMPG"].values[0]]]
-		});
-			let b = driver_df.getGroup([dfdriver.iloc({rows: [i]})["DriverName"].values[0]]);
-			const median = d3.quantile(b["CostPerMile"].values, 0.5);
-			const q1 = d3.quantile(b["CostPerMile"].values, 0.25);
-			const min = d3.min(b["CostPerMile"].values);
-			const max = d3.max(b["CostPerMile"].values);
-			const q3 = d3.quantile(b["CostPerMile"].values, 0.75);
-			driverdata.push({x: dfdriver.iloc({rows: [i]})["DriverName"].values[0], y: [min, q1, median, q3, max]})
-		}
-	}
 	var ontimedata = dfdriver["OnTimeRate"].values;
 	var mpgdata = dfdriver["MedianMPG"].values;
 	var mphdata = dfdriver["MedianSpeed"].values;

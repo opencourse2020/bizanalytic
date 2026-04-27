e301f993274e3caa8a3395092d1f3cfd46d03652
opencourse2020 <akaaboune@gmail.com>
2026-04-27 12:51:47 +0100

    new operations research module

--- bizanalytic/logiflex/views.py
+++ bizanalytic/logiflex/views.py
@@ -2601,6 +2601,7 @@ class AdvancedReportCreateView(LoginRequiredMixin, View, JsonFormMixin):
                 # --- Statistics ---
                 logireport.carrier_stats_json = carrier_stats
                 # logireport.driver_stats_json = driver_stats
+                print(driver_stats)
                 logireport.route_stats_json = route_stats
 
                 # --- Summary counts ---

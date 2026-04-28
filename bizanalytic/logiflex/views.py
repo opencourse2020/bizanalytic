9efb485fbee02a8c4f715fc8886e514ad9c5adbc
opencourse2020 <akaaboune@gmail.com>
2026-04-28 21:43:43 +0100

    new operations research module

--- bizanalytic/logiflex/views.py
+++ bizanalytic/logiflex/views.py
@@ -2615,11 +2615,13 @@ class AdvancedReportCreateView(LoginRequiredMixin, View, JsonFormMixin):
                 # --- Statistics ---
                 logireport.carrier_stats_json = carrier_stats
                 logireport.save()
-                # print(driver_stats)
+                print(driver_stats)
+                print("**************************************************************************")
+                print("**************************************************************************")
                 # if not isinstance(driver_stats, dict):
                 #     driver_stats = json.dumps(dict(driver_stats))
                 # else:
-                driver_stats = json.dumps(driver_stats, default=convert_to_python_types)
+                driver_stats = json.loads(json.dumps(driver_stats, default=convert_to_python_types))
                 print(driver_stats)
                 logireport.driver_stats_json = driver_stats
                 logireport.save()

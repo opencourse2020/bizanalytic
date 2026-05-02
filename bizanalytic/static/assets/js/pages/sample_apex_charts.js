15e746f5d0d3e401839d31745b591f3430a6cbfc
opencourse2020 <akaaboune@gmail.com>
2026-05-02 01:31:55 +0100

    new operations research module

--- bizanalytic/static/assets/js/pages/sample_apex_charts.js
+++ bizanalytic/static/assets/js/pages/sample_apex_charts.js
@@ -247,53 +247,85 @@ var routesheatdmadoptions = {
 			}
         };
 
-
     var fleetscorechartoptions = {
-        chart: {
-            height: 350,
-            type: "radialBar"
-        },
-
         series: [fleetscore],
-        colors: ["#c07008"],
+          chart: {
+          height: 350,
+          type: 'radialBar',
+          toolbar: {
+            show: true
+          }
+        },
         plotOptions: {
-            radialBar: {
-                hollow: {
-                    margin: 0,
-                    size: "70%",
-                    background: "#FAFAFA"
-                },
+          radialBar: {
+            startAngle: -135,
+            endAngle: 225,
+             hollow: {
+              margin: 0,
+              size: '70%',
+              background: '#fff',
+              image: undefined,
+              imageOffsetX: 0,
+              imageOffsetY: 0,
+              position: 'front',
+              dropShadow: {
+                enabled: true,
+                top: 3,
+                left: 0,
+                blur: 4,
+                opacity: 0.5
+              }
+            },
+            track: {
+              background: '#fff',
+              strokeWidth: '67%',
+              margin: 0, // margin is in pixels
+              dropShadow: {
+                enabled: true,
+                top: -3,
+                left: 0,
+                blur: 4,
+                opacity: 0.7
+              }
+            },
 
-                dataLabels: {
-                    showOn: "always",
-                    name: {
-                        offsetY: -10,
-                        show: true,
-                        color: "#000",
-                        fontSize: "13px"
-                    },
-                    value: {
-                        color: "#000",
-                        fontSize: "25px",
-                        show: true
-                    }
-                }
+            dataLabels: {
+              show: true,
+              name: {
+                offsetY: -10,
+                show: true,
+                color: '#888',
+                fontSize: '17px'
+              },
+              value: {
+                formatter: function(val) {
+                  return parseInt(val);
+                },
+                color: '#111',
+                fontSize: '36px',
+                show: true,
+              }
             }
+          }
         },
         fill: {
-            type: "gradient",
-            gradient: {
-              shade: "dark",
-              type: "vertical",
-              gradientToColors: ["#b91c1c"],
-              stops: [0, 100]
-            }
-          },
+          type: 'gradient',
+          gradient: {
+            shade: 'dark',
+            type: 'horizontal',
+            shadeIntensity: 0.5,
+            gradientToColors: ['#dc9d8a'],
+            inverseColors: true,
+            opacityFrom: 1,
+            opacityTo: 1,
+            stops: [0, 100]
+          }
+        },
         stroke: {
-            lineCap: "round",
+          lineCap: 'round'
         },
         labels: ['Score'],
-    };
+        };
 
 var fleetscorechart = new ApexCharts(document.querySelector("#fleetscorechart"), fleetscorechartoptions);
         fleetscorechart.render();
